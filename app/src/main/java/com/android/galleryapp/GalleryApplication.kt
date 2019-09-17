41800460e169b22ff6e374cb2f860cd449f79714
Adam Szkup <szkup.a@gmail.com>
2019-09-17 18:41:30 +0200

    add GalleryApp class, init timber and koin DI

---
+++ app/src/main/java/com/android/galleryapp/GalleryApplication.kt
@@ -0,0 +1,35 @@
+package com.android.galleryapp
+
+import android.app.Application
+import android.content.Context
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.module.Module
+import timber.log.Timber
+
+class GalleryApplication : Application() {
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        setupDependencyInjection()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    private fun setupDependencyInjection() {
+        startKoin {
+            androidLogger()
+            androidContext(this@GalleryApplication)
+            modules(appModules)
+        }
+    }
+}
+
+val appModules = mutableListOf<Module>()
