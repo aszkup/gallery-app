6e2e6873fb1fe1ff8ed2a19921945162cf0e11dd
Adam Szkup <szkup.a@gmail.com>
2019-09-21 11:00:22 +0200

    add room db

---
+++ app/src/main/java/com/android/galleryapp/data/AppDatabase.kt
@@ -0,0 +1,25 @@
+package com.android.galleryapp.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
+            }
+
+        private fun buildDatabase(context: Context): AppDatabase {
+            return Room.inMemoryDatabaseBuilder(context.applicationContext, AppDatabase::class.java).build()
+        }
+    }
+}
