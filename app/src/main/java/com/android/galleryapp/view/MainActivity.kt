600c03c19b33d6e9071c863b51df8cc9cb93ba65
Adam Szkup <szkup.a@gmail.com>
2019-09-22 10:34:16 +0200

    add drop menu

--- app/src/main/java/com/android/galleryapp/view/MainActivity.kt
+++ app/src/main/java/com/android/galleryapp/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.android.galleryapp.view
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import com.android.galleryapp.R
 
@@ -9,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.date_sort_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
